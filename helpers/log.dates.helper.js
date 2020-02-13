8ad40bfb7241f9e6c13d07d7c5487472c4c3b33c
Roberto Díaz <robertodzbt@gmail.com>
2020-02-13 19:57:19 +0100

    Helper folder removed

--- helpers/log.dates.helper.js
+++
@@ -1,9 +0,0 @@
-// Helper function that calculates log durations
-module.exports.updateLogDuration = (createdAt) => {
-  
-  const hoursFromStartingLog = Math.round((createdAt).setDate(formatDate.getDate()) / (1000 * 60 * 60 * 24));
-  const actualDate = Math.round((new Date()).setDate(new Date().getDate()) / (1000 * 60 * 60 * 24));
-  const ageInWeeks = Math.round((actualDate - daysFromLastPeriod) / 7);
-  
-  return ageInWeeks;
-}
