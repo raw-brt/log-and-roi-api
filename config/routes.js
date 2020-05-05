ad23bdda1729299e8a51b777bf0053d6af7e5b13
Roberto Díaz <robertodzbt@gmail.com>
2020-05-05 11:28:02 +0200

    Authentication middleware implemented

--- config/routes.js
+++ config/routes.js
@@ -3,29 +3,31 @@ const router             = express.Router();
 const usersController    = require('../controllers/users.controller');
 const projectsController = require('../controllers/projects.controller');
 const logsController     = require('../controllers/logs.controller');
+const authMiddleware     = require('../middlewares/auth.middleware');
 
 // Access routes
-router.post('/login', usersController.doLogin);
-router.post('/logout', usersController.logout);
+router.post('/login', authMiddleware.isNotAuthenticated, usersController.doLogin);
+router.post('/logout', authMiddleware.isAuthenticated, usersController.logout);
 
 // User routes
 router.get('/users/:username', usersController.profile);
-router.post('/users', usersController.create);
+router.post('/users', authMiddleware.isNotAuthenticated, usersController.create);
 router.patch('/users/:userId', usersController.update);
-router.delete('/users/:userId', usersController.delete);
+router.delete('/users/:userId', authMiddleware.isAuthenticated, usersController.delete);
 
 // Project routes
-router.post('/:userId/projects/new', projectsController.create);
-router.get('/user/:userId/projects', projectsController.read);
-router.get('projects/:projectId', projectsController.detail);
-router.patch('/projects/:projectId', projectsController.update);
-router.delete('/projects/:projectId', projectsController.delete);
+router.post('/:userId/projects/new', authMiddleware.isAuthenticated, projectsController.create);
+router.get('/', authMiddleware.isAuthenticated)
+router.get('/user/:userId/projects', authMiddleware.isAuthenticated, projectsController.read);
+router.get('projects/:projectId', authMiddleware.isAuthenticated, projectsController.detail);
+router.patch('/projects/:projectId', authMiddleware.isAuthenticated, projectsController.update);
+router.delete('/projects/:projectId', authMiddleware.isAuthenticated, projectsController.delete);
 
 // Log routes
-router.get('/:projectId/logs', logsController.read);
-router.get('/:logId/detail', logsController.detail);
-router.post('/:projectId/logs/new', logsController.create);
-router.patch('/logs/:logId', logsController.update);
-router.delete('/logs/:logId/delete', logsController.delete);
+router.get('/:projectId/logs', authMiddleware.isAuthenticated, logsController.read);
+router.get('/:logId/detail', authMiddleware.isAuthenticated, logsController.detail);
+router.post('/:projectId/logs/new', authMiddleware.isAuthenticated, logsController.create);
+router.patch('/logs/:logId', authMiddleware.isAuthenticated, logsController.update);
+router.delete('/logs/:logId/delete', authMiddleware.isAuthenticated, logsController.delete);
 
 module.exports = router;
